6f259280f7414dd80d0faaccd8ae4c73e9b20295
camziny <cameronziny@gmail.com>
2024-06-06 10:08:54 -0400

    - add shadcn sonner component

--- src/app/_components/simpleUploadButton.tsx
+++ src/app/_components/simpleUploadButton.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "../../utils/uploadthing";
+import { toast } from "sonner";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -49,7 +50,15 @@ function UploadSvg() {
 export function SimpleUploadButton() {
   const router = useRouter();
   const { inputProps } = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast("Uploading...", {
+        duration: 100000,
+        id: "upload-begin",
+      });
+    },
     onClientUploadComplete() {
+      toast.dismiss();
+      toast("Upload complete!");
       router.refresh();
     },
   });
